2b7bb890434942e9cc114c896da15f20d4979869
JoNoToPo <J.engelhard0@gmail.com>
2025-03-08 12:39:12 -0800

    Implement title

--- game.py
+++ game.py
@@ -7,18 +7,29 @@ def game():
     start_room = initialize.room_radomizer(8, 4)
     current_character = initialize.initialize_mob(initialize.new_character(), start_room)
     start_map = initialize.starting_map(start_room, 8, 4)
-    test_text = "-" * 30 + "/Your name is " + current_character["name"] + " /and you are going to rewrite History/" + "-" * 30 + "/JONTOTO//GODO"
+    menu_text = (
+            "-" * 53 +
+            "/,------.        ,--.   ,--.       ,--.  ,--."
+            "/|  .--. ' ,---. |  |   |  |,--.--.`--',-'  '-. ,---.  "
+            "/|  '--'.'| .-. :|  |.'.|  ||  .--',--.'-.  .-'| .-. : "
+            "/|  |\\  \\ \\   --.|   ,'.   ||  |   |  |  |  |  \\   --. "
+            "/`--' '--' `----''--'   '--'`--'   `--'  `--'   `----' /"
+            + "-" * 53 +
+            "//Your name is " + current_character["name"] + " /and you are going to rewrite History" +
+            "//To learn how to play type \"help\" at any time,"
+            "/or if you want more details about any action /type \"help (action)\""
+            " and it will be explained to you.")
     print(map.map_art(map.display_text_next_to_map(
-        map.rewrite_spot(start_map, current_character["x_coordinate"], current_character["y_coordinate"], 2), test_text,
-        2)))
+        map.rewrite_spot(start_map, current_character["x_coordinate"], current_character["y_coordinate"], 2), menu_text,
+        0)))
     while True:
-        player_input = str(input()).strip().lower()
+        player_input = str(input("To play type \"start\":")).strip().lower()
         if player_input:
             player.parse(player_input, current_character, start_map)
             print(map.map_art(map.display_text_next_to_map(
                 map.rewrite_spot(start_map, current_character["x_coordinate"], current_character["y_coordinate"], 2),
-                test_text,
-                2)))
+                menu_text,
+                0)))
         else:
             continue
 
