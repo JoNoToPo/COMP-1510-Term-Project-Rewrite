84cedfebe83360623c331b559382ecf083dcd467
JoNoToPo <J.engelhard0@gmail.com>
2025-04-05 14:01:22 -0700

    Correct docstrings

--- initialize.py
+++ initialize.py
@@ -7,10 +7,10 @@ def room_randomizer(max_size: int, min_size: int):
     """
     Randomizes the size and placement of a room.
 
-    :precondition: two integers
-    :postcondition: a dictionary
     :param max_size: an integer indicating the maximum size the room can be 1 < max_size < 29
     :param min_size: an integer indicating the minimum size the room can be 1 < min_size < 29
+    :precondition: two integers
+    :postcondition: a dictionary
     :return: a dictionary of non-zero length containing two integer coordinates in a tuple for each key and
     '   ' as the value
     """
@@ -26,13 +26,13 @@ def room_connector(first_room: dict, second_room: dict):
     """
     Connects two rooms with a hallway
 
-    :precondition: two dictionaries of non-zero length containing two integer coordinates in a tuple for each key and
-    '   ' as the value
-    :postcondition: one dictionary
     :param first_room: a dictionary of non-zero length containing two integer coordinates in a tuple for each key and
     '   ' as the value
     :param second_room: a dictionary of non-zero length containing two integer coordinates in a tuple for each key and
     '   ' as the value
+    :precondition: two dictionaries of non-zero length containing two integer coordinates in a tuple for each key and
+    '   ' as the value
+    :postcondition: one dictionary
     :return: a dictionary of non-zero length containing two integer coordinates in a tuple for each key and
     '   ' as the value
     >>> room_connector({(1, 1): "   "}, {(3, 3): "   "})
@@ -68,12 +68,14 @@ def starting_map(starting_room: dict, max_room_size: int, min_room_size: int, nu
     """
     Randomizes the initial ascii map for a floor
 
-    :precondition: a dictionary and three integers
-    :postcondition: a dictionary
     :param starting_room: a dictionary containing the coordinates of one rectangular room
     :param max_room_size: an integer indicating the maximum size the room can be 1 < max_size < 29
     :param min_room_size: an integer indicating the minimum size the room can be 1 < min_size < 29
     :param number_of_rooms: a positive non-zero integer indicating the amount of rooms that will be generated
+    :precondition: a dictionary and three integers
+    :postcondition: a dictionary
+    :return: the starting map a dictionary of non-zero length containing two integer coordinates in a tuple for each key and
+    '   ' for each value
     """
     while number_of_rooms != 1:
         starting_room = room_connector(starting_room, room_randomizer(max_room_size, min_room_size))
@@ -85,11 +87,12 @@ def initialize_mob(mob: dict, map_key: dict, start_room: dict):
     """
     Places a mob where there are no other mobs.
 
-    :precondition: three dictionaries
-    :postcondition: a dictionary
     :param mob: the mob dictionary with at minimum the keys y_coordinate and x_coordinate
     :param map_key: the map within which the mob is being placed
     :param start_room: the coordinates within which the mob is not allowed to be placed
+    :precondition: three dictionaries
+    :postcondition: a dictionary
+    :return: the mob dictionary with the coordinates randomized to be within the given map and outside the start room
 
     >>> initialize_mob({"y_coordinate": 0, "x_coordinate": 0}, {(0, 0): "   ", (3,3): "   "}, {(0, 0): "   "})
     {'y_coordinate': 3, 'x_coordinate': 3}
